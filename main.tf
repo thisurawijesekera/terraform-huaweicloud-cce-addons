08cc16a1f09be93cdba3e1bb09ef5db3117a3bb5
thisurawijesekera <98374770+thisurawijesekera@users.noreply.github.com>
2024-10-10 15:34:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -69,7 +69,6 @@ resource "huaweicloud_cce_addon" "nginx_ingress" {
             } : {}),
             var.nginx_ingress_extra_annotations
           )
-          type: "LoadBalancer"
         }
         tcp : {}
         udp : {}
